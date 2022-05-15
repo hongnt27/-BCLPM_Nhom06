e356b089f1f8d3c29c821199b91635361f586aa8
unknown <hongnt27>
2022-05-15 22:36:15 +0700

    nhom6

---
+++ src/main/java/com/qlsv/ptit/tinhdiemptit/TinhDiemPtitApplication.java
@@ -0,0 +1,12 @@
+package com.qlsv.ptit.tinhdiemptit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinhDiemPtitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinhDiemPtitApplication.class, args);
+	}
+}
